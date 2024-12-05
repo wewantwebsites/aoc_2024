6cf1ec9cda4843f365ba84048138720c4e42b45b
wewantwebsites <cas.guzdziol@wewantweb.site>
2024-12-05 01:43:31 -0500

    day2 part1 finished

---
+++ src/bin/day2.rs
@@ -0,0 +1,6 @@
+use aoc_2024::day2::{part1, part2};
+
+fn main() {
+    part1();
+    part2();
+}
---
+++ src/day2.rs
@@ -0,0 +1,61 @@
+use std::fs;
+
+enum Trend {
+    increasing,
+    decreasing,
+}
+
+pub fn part1() {
+    println!("Part 1: ");
+
+    let file = fs::read_to_string("inputs/day2.txt").expect("There was an issue reading the file.");
+    let mut total = 0;
+
+    'lines: for line in file.lines() {
+        let numbers: Vec<u32> = line
+            .split_whitespace()
+            .map(|s| {
+                let n: u32 = s.parse().unwrap();
+                n
+            })
+            .collect();
+
+        let trend;
+        if numbers[0] > numbers[numbers.len() - 1] {
+            trend = Trend::decreasing;
+        } else {
+            trend = Trend::increasing;
+        }
+
+        for i in 1..numbers.len() {
+            let prev = numbers[i - 1];
+            let curr = numbers[i];
+
+            match trend {
+                Trend::increasing => {
+                    if prev > curr {
+                        continue 'lines;
+                    }
+                }
+                Trend::decreasing => {
+                    if prev < curr {
+                        continue 'lines;
+                    }
+                }
+            }
+
+            let diff = curr.abs_diff(prev);
+            if diff < 1 || diff > 3 {
+                continue 'lines;
+            }
+        }
+
+        total = &total + 1;
+    }
+
+    println!("{}", total);
+}
+
+pub fn part2() {
+    println!("Part 2: ");
+}
